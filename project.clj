495a50ee2cfb2ba0ccd77324d7ac1669883ead54
Benjamin Teuber <bsteuber@gmail.com>
2012-11-20 14:56:12 +0100

    replace pom by project.clj with :aot compilation

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject com.gateprotect/data.json "0.2.1"
+  :description "Generating/parsing JSON from/to Clojure data structures.
+  AOT-compiled version."
+  :url "https://github.com/gateprotect/data.json"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :aot [#"^clojure.data.*"]
+  :source-paths ["src/main/clojure"]
+  :dependencies [[org.clojure/clojure "1.3.0"]])
